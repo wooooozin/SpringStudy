a4568a30386ec2a7ff4a5c13c75ba1e79de8c2d0
wooooozin <wooooozin@gmail.com>
2023-10-16 06:53:33 +0900

    컴포넌트 스캔과 자동 의존 관계 설정

--- hello-spring/src/main/java/com/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/com/hello/hellospring/repository/MemoryMemberRepository.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
--- hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/com/hello/hellospring/service/MemberService.java
@@ -2,14 +2,17 @@ package com.hello.hellospring.service;
 
 import com.hello.hellospring.domain.Member;
 import com.hello.hellospring.repository.MemberRepository;
-import com.hello.hellospring.repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
