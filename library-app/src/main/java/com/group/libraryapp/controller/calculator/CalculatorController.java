3c95f701dac2b8e1068b2e5084467fff12f5d1ab
wooooozin <wooooozin@gmail.com>
2023-10-17 07:31:34 +0900

    곱셉 POST API 구현

--- library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ library-app/src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,16 +1,26 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.calculator.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
 
-    @GetMapping("add")
+    @GetMapping("/add")
     public int addTwoNumbers(
         CalculatorAddRequest request
     ) {
         return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply")
+    public int multiplyNumbers(
+        @RequestBody CalculatorMultiplyRequest request
+    ) {
+        return request.getNumber1() * request.getNumber2();
     }
 }
