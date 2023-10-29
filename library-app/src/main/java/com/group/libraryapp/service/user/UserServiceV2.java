41cc47705b8592a8c8dc7c001a318f02916e5952
wooooozin <wooooozin@gmail.com>
2023-10-29 09:58:46 +0900

    트랜잭션 이해하기

--- library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ library-app/src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -8,6 +8,7 @@ import com.group.libraryapp.repository.user.UserRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceV2 {
@@ -18,10 +19,12 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         List<User> users = userRepository.findAll();
         return users.stream()
@@ -29,6 +32,7 @@ public class UserServiceV2 {
             .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId())
             .orElseThrow(IllegalArgumentException::new);
@@ -37,6 +41,7 @@ public class UserServiceV2 {
         userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name)
             .orElseThrow(IllegalArgumentException::new);
