800712d9655819395f1a6d3f2e5bb4abe1cc312a
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-01-14 17:46:08 +0900

    프론트 컨트롤러

---
+++ spring-mvc/servlet/src/main/java/hello/servlet/web/frontcotroller/v1/MemberListControllerV1.java
@@ -0,0 +1,27 @@
+package hello.servlet.web.frontcotroller.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV1 implements ControllerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String path = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ spring-mvc/servlet/src/main/java/hello/servlet/web/frontcotroller/v1/MemberSaveControllerV1.java
@@ -0,0 +1,31 @@
+package hello.servlet.web.frontcotroller.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(name, age);
+        memberRepository.save(member);
+
+        // Model에 데이터를 보관
+        request.setAttribute("member", member);
+
+        String path = "/WEB-INF/views/mvc-save.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+        dispatcher.forward(request, response);
+    }
+}
