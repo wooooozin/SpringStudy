ea38f05cb0ffd337d0a98aa76f6ba2e91aa7d542
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-01-27 13:04:10 +0900

    요청매핑

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,65 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class MappingController {
+
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(
+            @PathVariable ("userId") String userId,
+            @PathVariable ("orderId")  String orderId
+    ) {
+        log.info("mappingPath userId={}", userId);
+        log.info("mappingPath orderId={}", orderId);
+        return "ok";
+    }
+
+    @GetMapping("mapping-param")
+    public String mappingParam(
+            @RequestParam("mode") String mode
+    ) {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    @GetMapping("mapping-header")
+    public String mappingHeader(
+        @RequestHeader("headers") String header
+    ) {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+}
