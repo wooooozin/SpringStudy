7eb576c2ecc517e81ca4266f8f80549c012c270c
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-01-30 05:30:40 +0900

    쿼리파라미터

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,24 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class RequestParamController {
+
+  @RequestMapping("/request-param-v1")
+  public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String username = request.getParameter("username");
+    int age = Integer.parseInt(request.getParameter("age"));
+    log.info("username = {}. gge = {}", username, age);
+
+    response.getWriter().write("ok");
+  }
+
+  
+}
