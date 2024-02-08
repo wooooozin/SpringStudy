25c65c8799dcd130d73ce752b765c1caed447425
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-02-09 08:11:30 +0900

    상품목록 - 타임리프

---
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,35 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.domain.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/basic/items")
+public class BasicItemController {
+
+  private final ItemRepository itemRepository;
+
+  /**
+   * 테스트용 데이터 추가
+   */
+  @PostConstruct
+  public void init() {
+    itemRepository.save(new Item("베이글", 10000, 10));
+    itemRepository.save(new Item("우유", 3800, 10));
+  }
+
+  @GetMapping
+  public String items(Model model) {
+    List<Item> items = itemRepository.findAll();
+    model.addAttribute("items", items);
+    return "basic/items";
+  }
+}
