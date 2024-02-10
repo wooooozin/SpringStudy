7722c65b9d08c8b3f1216193655de06bbd5293bb
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-02-10 18:09:25 +0900

    상품 상세 api

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,4 +33,13 @@ public class BasicItemController {
     model.addAttribute("items", items);
     return "basic/items";
   }
+
+  @GetMapping("/{itemId}")
+  public String item(@PathVariable Long itemId, Model model) {
+    Item item = itemRepository.findById(itemId);
+    model.addAttribute("item", item);
+    return "basic/item";
+  }
+
+
 }
