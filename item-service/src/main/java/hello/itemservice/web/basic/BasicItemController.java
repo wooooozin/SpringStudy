a1e6c65b6997ecb50ce83d439d5d40634602733e
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-02-13 05:44:48 +0900

    상품등록 - @ModelAttribute

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -47,8 +49,27 @@ public class BasicItemController {
     return "basic/addForm";
   }
 
+//  @PostMapping("/add")
+//  public String save(
+//      @RequestParam String itemName,
+//      @RequestParam Integer price,
+//      @RequestParam Integer quantity,
+//      Model model
+//  ) {
+//    Item item = new Item(itemName, price, quantity);
+//    itemRepository.save(item);
+//    model.addAttribute("item", item);
+//    return "basic/item";
+//  }
+
   @PostMapping("/add")
-  public String save() {
-    return "basic/addForm";
+  public String addV1(
+      @ModelAttribute("item") Item item,
+      Model model
+  ) {
+
+    itemRepository.save(item);
+//    model.addAttribute("item", item);
+    return "basic/item";
   }
 }
