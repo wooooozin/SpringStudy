ac0ce50c8a8c887f35dd047747f40b870248dba2
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-02-13 05:33:39 +0900

    상품 등록

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -41,5 +42,13 @@ public class BasicItemController {
     return "basic/item";
   }
 
+  @GetMapping("/add")
+  public String addForm() {
+    return "basic/addForm";
+  }
 
+  @PostMapping("/add")
+  public String save() {
+    return "basic/addForm";
+  }
 }
