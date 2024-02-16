8aec20aaf389e3097080f65e3b29c81704d91afe
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-02-17 07:10:37 +0900

    프로젝트 생성

---
+++ myblog-exam/example-spring/src/main/java/com/woozi/com/woozi/ExampleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.woozi.com.woozi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringApplication.class, args);
+	}
+
+} 
---
+++ myblog-exam/example-spring/src/test/java/com/woozi/ExampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woozi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
