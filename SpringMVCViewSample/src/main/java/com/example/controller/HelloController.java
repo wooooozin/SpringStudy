fe556d5323c9494c3bb008a38f80004ff30a737c
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-02-26 06:07:05 +0900

    샘플 mvc

---
+++ SpringMVCViewSample/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("/view")
+  public String helloView() {
+    return "hello.html";
+  }
+}
---
+++ SpringMVCViewSample/src/test/java/com/example/SpringMVCViewSample/SpringMvcViewSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVCViewSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcViewSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
