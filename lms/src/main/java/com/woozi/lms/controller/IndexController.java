ef79d5a4c89005d87c1a151e352de8b25e9721cc
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-04-16 04:41:27 +0900

    프로젝트 생성 및 테스트 연결

---
+++ lms/src/main/java/com/woozi/lms/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.woozi.lms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+  @GetMapping("/")
+  public String testIndexPage(
+      Model model
+  ) {
+    model.addAttribute("welcome", "welcome to the site");
+    return "index";
+  }
+
+}
