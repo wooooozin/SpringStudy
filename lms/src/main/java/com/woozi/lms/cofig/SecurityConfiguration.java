269c3b72f29030cbf7f816da30197aa0809bd18d
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-04-30 05:45:22 +0900

    스프링 시큐리티

---
+++ lms/src/main/java/com/woozi/lms/cofig/SecurityConfiguration.java
@@ -0,0 +1,12 @@
+package com.woozi.lms.cofig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecu {
+
+
+}
